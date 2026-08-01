94f48c247ae61be84e3cc4a31fbc971b5c3b4441
Renz Frederick <d3nzus@gmail.com>
2026-08-01 21:45:56 +0800

    feat: import tailwind

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="h-screen mx-auto flex flex-col justify-center items-center bg-black text-white">
+            <h1>Resdyssey</h1>
+        </div>
+    )
+};
+
+export default App;
